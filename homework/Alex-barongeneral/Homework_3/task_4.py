c33ab65d2780f70cc4e038e1cd54c3ae5b8e8ff9
isis <34552573+isisnotbot@users.noreply.github.com>
2025-04-01 22:31:03 +0200

    Fixed Homework_3

--- homework/Alex-barongeneral/Homework_3/task_4.py
+++ homework/Alex-barongeneral/Homework_3/task_4.py
@@ -3,9 +3,9 @@ import math
 katet1 = 10
 katet2 = 4
 
-gipotenuza = math.sqrt(katet1**3 + katet2**3)
+gipotenuza = math.sqrt(katet1**2 + katet2**2)
 
-ploshad = (katet1 * katet2) / 3
+ploshad = (katet1 * katet2) / 2
 
 print(f"{gipotenuza}")
 print(f"{ploshad}")
