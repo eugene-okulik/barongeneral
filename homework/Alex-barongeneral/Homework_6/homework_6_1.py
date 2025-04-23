a75ef0cb8fa37f4ce38a5f83462cde9dbdf89416
isis <34552573+isisnotbot@users.noreply.github.com>
2025-04-23 15:56:00 +0200

    Homework_6

--- homework/Alex-barongeneral/Homework_6/homework_6_1.py
+++ homework/Alex-barongeneral/Homework_6/homework_6_1.py
@@ -1,14 +1,11 @@
 text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
 
-words = text.split()
-new_words = []
+result = ""
 
-for word in words:
-    if word[-1] in [",", "."]:
-        new_word = word[:-1] + "ing" + word[-1]
+for word in text.split():
+    if word[-1] in ",.":
+        result += word[:-1] + "ing" + word[-1] + " "
     else:
-        new_word = word + "ing"
-    new_words.append(new_word)
-
-new_text = " ".join(new_words)
-print(new_text)
+        result += word + "ing "
+        
+print(result.strip())
